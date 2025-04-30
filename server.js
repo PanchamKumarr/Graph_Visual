96c693be725e69ab35ac5e80acee7c7db96a1d89
PanchamKumarr <pancham8256@gmail.com>
2025-05-01 01:34:29 +0530

    Updated server.js to work in both development and production environments

--- server.js
+++ server.js
@@ -10,5 +10,13 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// Start server in development mode
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
+}
+
 // Export the Express app for Vercel
 module.exports = app;
